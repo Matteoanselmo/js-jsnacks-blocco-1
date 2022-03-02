407ae5d6f82e5c3562b7c531016658736cf01a98
Matteo Anselmo <matteo.anselmo96@gmail.com>
2022-03-02 12:16:04 +0100

    correzione {

--- jsnack-3(1)/js/script.js
+++ jsnack-3(1)/js/script.js
@@ -9,10 +9,12 @@ while(contatore < 200){
     contatore += userNumber;
     if(userNumber % 2 == 0){
         myPariArray.push(userNumber);
-    }else
-    myDispariArray.push(userNumber);
+    }else{
+        myDispariArray.push(userNumber);
+    }
+    
+
 }
 
 console.log(myPariArray);
 console.log(myDispariArray);
-console.log(userNumber);
