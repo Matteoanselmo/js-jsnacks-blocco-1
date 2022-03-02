ad00f68e7e0c6e2f0c33c96240cb9f6a6b6643f5
Matteo Anselmo <matteo.anselmo96@gmail.com>
2022-03-02 12:22:58 +0100

    correione variabile userNumber

--- jsnack-3(1)/js/script.js
+++ jsnack-3(1)/js/script.js
@@ -1,20 +1,18 @@
 const myPariArray = [];
 const myDispariArray = []; 
 
-let userNumber = parseInt( prompt('Inserisci un umero '));
 let contatore = 0; 
 
 while(contatore < 200){
-    userNumber = parseInt( prompt('Inserisci un umero '));
+    let userNumber = parseInt( prompt('Inserisci un umero '));
     contatore += userNumber;
     if(userNumber % 2 == 0){
         myPariArray.push(userNumber);
     }else{
         myDispariArray.push(userNumber);
-    }
-    
-
-}
+    };
+    console.log('la somma è: ' + contatore);
+};
 
 console.log(myPariArray);
 console.log(myDispariArray);
